137331eb7e8e5b53bc0554db04c8bb9ba5431d8c
SaDR4M <sadraagjl04@gmail.com>
2026-02-23 17:01:10 +0330

    Update(account): Add url versioning

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,6 @@
-"""
-URL configuration for authentication project.
+"""URL configuration for authentication project."""
 
+"""
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 from rest_framework import permissions
+from rest_framework.routers import SimpleRouter
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
@@ -35,9 +36,14 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('account/' , include("account.urls")),
-       path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+urlpatterns = [
+    path("api/v1/", include(urlpatterns))
+]
+
 
 
