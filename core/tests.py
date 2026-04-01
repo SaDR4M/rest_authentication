d7ecf245d5b6436de5137b8b3240d9cd65aca385
SaDR4M <sadraagjl04@gmail.com>
2026-04-01 16:31:06 +0330

    Feat(core): Test for database and cache health
    - Transaction test case for database to test the production database
    - Simple test case for cache service to test the health of the cache

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.test import SimpleTestCase, TransactionTestCase
+from redis.connection import Connection as RedisConnection, ConnectionError
+from django.db import connection as db_connection , OperationalError
 
 # Create your tests here.
+class DataBaseConnectionTestCase(TransactionTestCase):
+    """This test case is for testing the health of the database (postgres) and it will use the main database that we use in the settings"""
+
+    def test_database_connection(self):
+        try:
+            connection = db_connection.ensure_connection()
+            with db_connection.cursor() as conn:
+                conn.execute("SELECT 1")
+        except OperationalError:
+            self.fail(msg="Database connection failed. check the Database")
+        except Exception as e:
+            self.fail(f"An unexpected error occurred during database connection test: {e}")
+        
+
+class CacheConnectionTestCase(SimpleTestCase):
+
+    def test_cache_connection(self):
+        try:
+            connection = RedisConnection().connect()
+        except ConnectionError:
+            self.fail(msg="Cache service is not available")
+        except Exception as e:
+            self.fail(f"An unexpected error occurred during redis connection test: {e}")
+            
